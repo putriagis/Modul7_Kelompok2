3594d85f05278d8da340425bb998910a9f77ebc2
thatFarhan <fabdillahmu@gmail.com>
2026-04-15 07:48:05 +0700

    menambahkan class FilmFavorit_103022400115

---
+++ FilmFavorit_103022400115.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class FilmFavorit_103022400115
+{
+
+		public string title { get; set; }
+		public string director { get; set; }
+		public string year { get; set; }
+		public string genre { get; set; }
+		public int rating { get; set; }
+		public int durationMinutes { get; set; }
+		public bool isWatched { get; set; }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Text.Json;
+
+namespace Modul7_Kelompok2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadJSON_103022400115();
+        }
+
+        static void ReadJSON_103022400115()
+        {
+            string json = File.ReadAllText("D:\\Modul7_Kelompok2\\jurnal7_1_103022400115.json");
+            var data = JsonSerializer.Deserialize<FilmFavorit_103022400115>(json);
+            Console.WriteLine("Judul: " + data.title);
+            Console.WriteLine("Sutradara: " + data.director);
+            Console.WriteLine("Tahun: " + data.year);
+            Console.WriteLine("Genre: " + data.genre);
+            Console.WriteLine("Rating: " + data.rating);
+            Console.WriteLine("Durasi: " + data.durationMinutes + " menit");
+            Console.WriteLine("isWatched: " + data.isWatched);
+        }
+    }
+}
