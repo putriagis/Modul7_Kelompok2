2420abb9bcc02e88a43e650daeca21ca454e62fd
putriagis <putribalqisnurhayati@gmail.com>
2026-04-15 07:52:50 +0700

    menambahkan class FilmFavorite_103022400081

---
+++ FilmFavorite_103022400081.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul7_Kelompok2
+{
+    public class FilmFavorite_103022400081
+    {
+        public string title { get; set; }
+        public string direction { get; set; }
+        public string year { get; set; }
+        public string genre { get; set; }
+        public int rating{ get; set; }
+        public int durationMinutes { get; set; }
+        public bool isWatched { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Modul7_Kelompok2;
+using System.Text.Json;
+
+namespace Modul7_kelompok2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            readJSON_putri();
+            Console.WriteLine();
+
+            static void readJSON_putri()
+            {
+                string json = File.ReadAllText("jurnal7_1_103022400081");
+                var data = JsonSerializer.Deserialize<FilmFavorite_103022400081>(json);
+                Console.WriteLine("Title: " + data.title);
+                Console.WriteLine("Direction: " + data.direction);
+                Console.WriteLine("Year: " + data.year);
+                Console.WriteLine("Genre: " + data.genre);
+                Console.WriteLine("Rating: " + data.rating);
+                Console.WriteLine("Duration Minutes: " + data.durationMinutes);
+                Console.WriteLine("Is Watched: " + data.isWatched);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
