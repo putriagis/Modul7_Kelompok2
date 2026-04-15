804c433eff98ecaae5c819da4e75505f9fac3918
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-04-15 07:35:54 +0700

    menambahkan class FilmFavorit_103022400095

---
+++ FilmFavorit_103022400095.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul7_Kelompok2
+{
+    internal class FilmFavorit_103022400095
+    {
+        public string title { get; set; }
+        public string director { get; set; }
+        public string year { get; set; }
+        public int rating { get; set; }
+        public int durationMinutes { get; set; }
+        public bool isWatched { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Modul7_Kelompok2;
+using System.Text.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ReadJSON1_103022400095();
+    }
+
+    static void ReadJSON1_103022400095()
+    {
+        Console.WriteLine("Hani - Film Favorit");
+        String json = File.ReadAllText("jurnal7_1_103022400095.json");
+
+        var data = JsonSerializer.Deserialize<FilmFavorit_103022400095>(json);
+
+        Console.WriteLine("Title: " + data.title);
+        Console.WriteLine("Director: " + data.director);
+        Console.WriteLine("Year: " + data.year);
+        Console.WriteLine("Rating: " + data.rating);
+        Console.WriteLine("Duration (minutes): " + data.durationMinutes);
+        Console.WriteLine("Watched: " + data.isWatched);
+
+    }
+}
